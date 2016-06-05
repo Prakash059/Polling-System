2f0afd37c8c28857b6db51413bfd26ae78e631b4
T.PRAKASH <tprakash059@gmail.com>
2016-06-05 22:37:23 +0530

    first commit

---
+++ student.php
@@ -0,0 +1,27 @@
+<?php
+mysql_connect('localhost', 'root', 'username') or die(mysql_error());
+mysql_select_db('polling') or die(mysql_error());
+
+session_start();
+//If your session isn't valid, it returns you to the login screen for protection
+if(empty($_SESSION['member_id'])){
+ header("location:access-denied.php");
+}
+?>
+<html><head>
+<link href="css/user_styles.css" rel="stylesheet" type="text/css" />
+</head><body bgcolor="tan"> 
+<center><b><font color = "brown" size="6">Online Voting System for Club Activities</font></b></center><br><br>
+<div id="page">
+<div id="header">
+<h1>STUDENT HOME </h1>
+<a href="student.php">Home</a> | <a href="vote.php">Current Polls</a> | <a href="manage-profile.php">Manage My Profile</a> | <a href="logout.php">Logout</a>
+</div>
+<div id="container">
+<p> Click a link above to do some stuff.</p>
+</div>
+<div id="footer">
+
+</div>
+</div>
+</body></html>
